eb04576a7b6ba2789003f488bcb92aae1435b5a9
Xiangrong Chen <xcn@mm-software.com>
2018-04-08 18:25:32 +0800

    Init

--- src/app/record/record.component.ts
+++ src/app/record/record.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {Store, select} from '@ngrx/store';
+import {Observable} from 'rxjs/Observable';
+import {CHANGE_FONT_SIZE} from '../redux/actions/canvas';
 
 @Component({
   selector: 'app-record',
@@ -6,13 +9,19 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./record.component.css']
 })
 export class RecordComponent implements OnInit {
+  settings: Observable<CanvasSettings>;
   message: string;
 
-  constructor() {
+  constructor(private store: Store<CanvasSettings>) {
+    this.settings = store.pipe(select('canvasSettings'));
     this.message = 'Bruce Chen';
   }
 
   ngOnInit() {
+    this.store.dispatch({
+      type: CHANGE_FONT_SIZE,
+      payload: 18
+    });
   }
 
   onClick() {
